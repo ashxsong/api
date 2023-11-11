f541a5475ce30dc4c09bfb67429cdb4902699d99
ashxsong <beethoven6198@gmail.com>
2023-11-11 14:57:27 -0600

    app

--- Api/App.js
+++ Api/App.js
@@ -1,8 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import BackgroundImage from './image/background.jpg';
+import axios from "axios";
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
+  const URL = 'https://coffee.alexflipnote.dev/'
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+		const fetchData = async () => {
+			console.log("Making GET Request");
+			try {
+				const response = await axios.get(URL)
+						 .then((response) => {
+						console.log(response)
+				}).catch(() => {
+          setData(response.data)
+				})
+			} catch (error) {
+        console.error('Failed to fetch data: ', error);
+			}
+		}
+
+		fetchData()
+	}, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Coffee API</Text>
@@ -10,6 +32,20 @@ export default function App() {
     </View>
   );
 }
+
+const renderItem = ({ item }) => {(
+  <ComponentForEachItem name={item.name} />
+)
+  return (
+    <View style={{ flex: 1 }}>
+        <FlatList
+          data={data}
+          keyExtractor={(item, index) => index.toString()} 
+          renderItem={renderItem}
+        />
+				{data.map((item) => <ComponentForEachItem name={item.name} />)}
+    </View>
+)}
 
 const styles = StyleSheet.create({
   container: {
@@ -22,6 +58,6 @@ const styles = StyleSheet.create({
     marginBottom: 400,
     fontSize: 40,
     fontFamily: 'Verdana',
-    color: 'SaddleBrown'
+    color:'#8B4513'
   },
 });
